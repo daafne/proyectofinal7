52aec1c94faaefed87096bf20acab7aae1e6f34d
daafne <115214517+daafne@users.noreply.github.com>
2023-05-30 20:38:02 +0200

    jj
    jj

--- js/switch.js
+++ js/switch.js
@@ -10,3 +10,14 @@ console.log(checkbox);
 checkbox.addEventListener("click", function(){
     document.body.classList.toggle("dark");
 })
+    const SwitchSec = document.querySelector(".switchAlt")
+    const sol = document.querySelector(".sol");
+    const luna = document.querySelector(".luna");
+    
+    SwitchSec.addEventListener("click", function(){
+    sol.classList.toggle("solEsconder")
+    luna.classList.toggle("lunaActivar");
+    body.classList.toggle("dark");
+
+
+})
